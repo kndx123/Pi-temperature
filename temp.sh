03ba2a03cdfe79cad38492069c34621565927ea5
Artur Kraak <314146@gmail.com>
2021-02-06 17:17:08 +0200

    Update temp.sh

--- temp.sh
+++ temp.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 while true
 do
-cpu=$(/opt/vc/bin/vcgencmd measure_temp | sed "s/\<temp=//g" | sed "s/..$//")
+cpu=$(/opt/vc/bin/vcgencmd measure_temp | egrep -o '[0-9]*\.[0-9]*')
 redis-cli set cpu $cpu
 sleep 1
 done
