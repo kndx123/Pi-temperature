18dc966c44309ce794a9e7ec46a479875900c0ea
Artur Kraak <314146@gmail.com>
2021-02-08 19:20:56 +0200

    Update temp.sh

--- temp.sh
+++ temp.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
 while true
 do
-cpu=$(/opt/vc/bin/vcgencmd measure_temp | sed "s/\<temp=//g" | sed "s/..$//")
-listlen=$(redis-cli llen my_list)
-
-if [[ $listlen > 10 ]]; then
-        redis-cli lpop my_list
-fi
-redis-cli rpush my_list $cpu
-sleep 1
+        cpu=$(/opt/vc/bin/vcgencmd measure_temp | sed "s/\<temp=//g" | sed "s/..$//")
+        listlen=$(redis-cli llen my_list)
+        if (( $listlen > 9 )); then
+                redis-cli lpop my_list
+        fi
+        redis-cli rpush my_list $cpu
+        sleep 1
 done
